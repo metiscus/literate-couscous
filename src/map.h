1982ac711b81832437d3c3f6f7f1c36fdaf30235
Michael Bosse <metiscus@gmail.com>
2017-12-21 16:54:31 -0500

    Adding in functions for map

--- src/map.h
+++ src/map.h
@@ -13,10 +13,19 @@ class Map
     int32_t height_;
     int32_t depth_;
 
-public:
+private:
     bool inbounds(Position position) const;
     bool inbounds_z(Position position) const;
 
     SubMap* get_submap_at(Position position);
     SubMap* get_submap_at(int32_t x, int32_t y);
+
+public:
+    bool passable(int32_t x, int32_t y) const;
+    bool impassable(int32_t x, int32_t y) const;
+    int32_t move_cost(int32_t x, int32_t y) const;
+
+
+    MapTile& maptile_at(Position position);
+    const MapTile& maptile_at(Position position) const;
 };
